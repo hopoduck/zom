7468e44cef24feaa3bead21da7236b88d95f8e12
Hopoduck <gksqudwh1@naver.com>
2022-04-10 15:30:34 +0900

    2.4 rooms

--- src/server.js
+++ src/server.js
@@ -15,11 +15,9 @@ const server = http.createServer(app);
 const io = SocketIO(server);
 
 io.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 5000);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    done();
   });
 });
 
